70e628cf12224fa1b9ca5096655714adf2f62f98
geobreed <geobreed@gmail.com>
2022-08-20 22:19:44 +0700

    Create suffix_stats.sh

---
+++ suffix_stats.sh
@@ -0,0 +1,6 @@
+echo 'Markdown lines: '
+find ./wiki/ -name "*.md"| xargs wc -l | tail -n 1
+echo 'Markdown word count: '
+find ./wiki/ -name "*.md"| xargs wc -w | tail -n 1
+echo '# of Markdown files: '
+find ./wiki/ -name "*.md"|wc -l
